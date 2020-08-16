f776af1dbc30b93f8e003967cf614060f8756c3f
ElGa7ed <you@example.com>
2020-08-17 00:25:44 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Online Appointment System
+A fully working online appointment system built on ASP.Net MVC 5 using C#. 
+Users can register an account, create appointments, view appointment history and change password. 
+Doctors can view upcoming appointments and appointment history. 
+Admin accounts can create profiles for doctors, edit profiles, change appointment durations and change the working hours from the front end.
+Microsoft Identity 1 is used for user accounts and all passwords are stored as hashes. 
+
+# [Live Demo on Microsoft Azure](http://greenhospital.azurewebsites.net/)
+# [Live Demo on Somee.com](http://greenhospital.somee.com/)
+
+The MIT License (MIT)
+
+Copyright (c) 2016 Adeel Ahmad
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using DoctorPatient.Infrastructure;
+using DoctorPatient.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoctorPatient.Startup))]
+namespace DoctorPatient
+{
+    public partial class Startup
+    {
+        private HospitalDbContext db = new HospitalDbContext();
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRole();
+            
+        }
+    
+        public void CreateRole()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            IdentityRole role;
+            
+            if (!roleManager.RoleExists("Admin"))
+            {
+                role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+            }
+            if (!roleManager.RoleExists("Doctor"))
+            {
+                role = new IdentityRole();
+                role.Name = "Doctor";
+                roleManager.Create(role);
+
+            }
+            if (!roleManager.RoleExists("Patient"))
+            {
+                role = new IdentityRole();
+                role.Name = "Patient";
+                roleManager.Create(role);
+
+            }
+            else
+            {
+                
+            }
+        }
+  
+
+    }
+}
+
